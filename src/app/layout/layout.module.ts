38e890d070e1c0138715e10bfd72bec65eaedb7b
vero-ball <v.rivadulla.deschamps@gmail.com>
2019-11-19 20:09:34 +0100

    router

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,13 +4,15 @@ import { ShellComponent } from './shell/shell.component';
 import { TopBarComponent } from './shell/top-bar/top-bar.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { CopyrightComponent } from './shell/copyright/copyright.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [ShellComponent, TopBarComponent, MainContentComponent, CopyrightComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [ShellComponent]
 })
