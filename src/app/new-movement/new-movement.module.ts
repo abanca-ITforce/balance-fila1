816e39818730b402da538852c5d6bc9801df50c2
vero-ball <v.rivadulla.deschamps@gmail.com>
2019-11-21 18:34:21 +0100

    Formulario

--- src/app/new-movement/new-movement.module.ts
+++ src/app/new-movement/new-movement.module.ts
@@ -3,13 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { NewMovementRoutingModule } from './new-movement-routing.module';
 import { NewMovementComponent } from './new-movement.component';
+import { FormMovementComponent } from './form-movement/form-movement.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
-  declarations: [NewMovementComponent],
+  declarations: [NewMovementComponent, FormMovementComponent],
   imports: [
     CommonModule,
-    NewMovementRoutingModule
+    NewMovementRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ]
 })
 export class NewMovementModule { }
